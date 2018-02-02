7d5d41890f4cab8061838457ee10ab2726e3c744
Rebecca Popper <rpopper7@gmail.com>
2018-02-01 20:52:09 -0500

    Finish Question 1

--- CSharpStructuresAlgorithms/Program.cs
+++ CSharpStructuresAlgorithms/Program.cs
@@ -5,13 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpStructuresAlgorithms
+namespace Chapter1
 {
     class Program
     {
         public static void Main(string[] args)
         {
             // For now, run things from other classes here to test.
+            //Ch1Q1_IsUnique newClass = new Ch1Q1_IsUnique();
+           // newClass.Run();
         }
     }
 }
--- CSharpStructuresAlgorithms/src/CrackingTheCodingInterview/Ch1/Ch1Q1_IsUnique.cs
+++ CSharpStructuresAlgorithms/src/CrackingTheCodingInterview/Ch1/Ch1Q1_IsUnique.cs
@@ -15,7 +15,7 @@ namespace Chapter1
 {
     class Ch1Q1_IsUnique
     {
-        // Solution 1: Using a HashSet
+        // Solution 1: Using a HashSet O(n) complexity
         // Why:  HashSet.Add will skip a new item if it's deemed equal to one of the existing items
         public bool IsUniqueSolution1(string str)
         {
@@ -35,8 +35,8 @@ namespace Chapter1
 
         // Solution 2: Using a Boolean Array **BOOK ANSWER**
         // Why: Ask if the string is ASCII or Unicode to set up flagging system
-        // ASCII - 256 extended characters
-        public bool IsUniqueSolution2(String str)
+        // ASCII - 256 extended characters.  O(1) time complexity for fixed dataset
+        public bool IsUniqueSolution2(string str)
         {
             if (str.Length > 256)          //  String must duplicate letters length exceeds alphabet       
             {
@@ -64,7 +64,7 @@ namespace Chapter1
 
             foreach (var word in words)
             {
-                Console.WriteLine(word + ": " + IsUniqueSolution1(word));
+                Console.WriteLine(word + ": " + IsUniqueSolution1(word) + " " + IsUniqueSolution2(word));
             }
         }
     }
